f582c2acb3d87fb10b5e299afb5f1fb7a9a2b625
elicost4 <eliabecostaa@outlook.com>
2025-05-24 14:34:18 -0300

    game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -24,7 +24,11 @@ public class Game { //object game
 	private String platforms;
 	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
